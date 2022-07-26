4d421adf675c7ec7f1dcd1a50143e95d9cf801ee
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-07-26 17:11:45 -0500

    add cockroachdb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+require('dotenv').config();
 
 @Module({
   controllers: [],
   providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'cockroachdb',
+      host: process.env.DB_HOST,
+      port: 26257,
+      username: process.env.DB_USER,
+      ssl: true,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      extra: {
+        options: `--cluster=${process.env.DB_CLUSTER}`,
+      },
+      synchronize: false,
+    }),
+  ],
 })
 export class AppModule {}
